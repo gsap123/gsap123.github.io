ad96cd41ab9b5d1c4af63891beb207c0b72337ee
gsap123 <112829460+gsap123@users.noreply.github.com>
2022-09-06 16:46:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ if (startButton){
 if (nextButton){
   nextButton.addEventListener('click', () => {
     currentQuestionIndex++
+    atOptions = {
+		'key' : 'b1198f294e45f01a544b42c65480755d',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	  };
+	  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/b1198f294e45f01a544b42c65480755d/invoke.js"></scr' + 'ipt>');
     setNextQuestion()
   })
 }
